cadea879767edc089e823e7ccd8826340856a1fc
osboxes <osboxes.org>
2020-01-22 05:31:51 -0500

    test

--- server/test/index.js
+++ server/test/index.js
@@ -120,6 +120,8 @@ describe('Datasets', () => {
                 item.push(j);
 
             }
+            expect(item[0]).to.equals('fid');
+            expect(item[1]).to.equals('name');
             expect(item[2]).to.equals('hombres');
 
         }
@@ -138,43 +140,69 @@ describe('Datasets', () => {
                   item.push(j);
 
               }
+              expect(item[0]).to.equals('cod');
+              expect(item[1]).to.equals('nom');
               expect(item[2]).to.equals('mujeres');
 
           }
         });
     });
 
-  describe('/GET dataset by params', () => {
-        it('it should GET dataset with params', (done) => {
-          chai.request(server)
-              .get('/api/datasets/p_poblacion17/data/total')
-              .end((err, res) => {
-                    res.should.have.status(200);
-                done();
-              });
+  describe('/GET dataset by params', function() {
+        it('it should GET dataset with params', async() => {
+          var response = await fetch('http://localhost:3000/api/datasets/p_poblacion17/data/total')
+          expect(response.status).to.be.equal(200);
+          var d = await response.json();
+          for (var i in d){
+              var datos = d[i];
+              var item = [];
+              for (var j in datos){
+                  item.push(j);
+
+              }
+              expect(item[0]).to.equals('cod');
+              expect(item[1]).to.equals('total');
+
+
+          }
         });
   });
 
 
-  describe('/GET  total items', () => {
-        it('it should GET total items', (done) => {
-          chai.request(server)
-              .get('/api/datasets/p_poblacion17/total_item/Mujeres')
-              .end((err, res) => {
-                    res.should.have.status(200);
-                done();
-              });
+  describe('/GET  total items', function() {
+        it('it should GET total items', async() => {
+          var response = await fetch('http://localhost:3000/api/datasets/p_poblacion18/total_item/Mujeres')
+          expect(response.status).to.be.equal(200);
+          var d = await response.json();
+          var total_res = d[0].sum;
+          var response = await fetch('http://localhost:3000/api/datasets/p_poblacion18/item/Mujeres')
+          expect(response.status).to.be.equal(200);
+          var d = await response.json();
+          var data_m = 0;
+          for (var i in d){
+              var datos = d[i];
+
+              for (var j in datos){
+                  if (j == 'mujeres'){
+                    data_m = data_m + parseInt(datos[j]);
+                  }
+
+              }
+          }
+          expect(parseInt(total_res)).to.equals(data_m);
+
+
         });
   });
 
-  describe('/GET  data territorial entity', () => {
-        it('it should GET data territorial entity', (done) => {
-          chai.request(server)
-              .get('/api/datasets/p_poblacion17/data/total/28')
-              .end((err, res) => {
-                    res.should.have.status(200);
-                done();
-              });
+  describe('/GET  data territorial entity', function() {
+        it('it should GET data territorial entity', async() => {
+          var response = await fetch('http://localhost:3000/api/datasets/p_poblacion17/data/id/28')
+          expect(response.status).to.be.equal(200);
+          var d = await response.json();
+          expect(d[0].cod).to.equals('28');
+          expect(d[0].año).to.equals('2017');
+          console.log(d)
         });
   });
 
@@ -189,26 +217,38 @@ describe('Datasets', () => {
         });
   });
 
-  describe('/GET data of map', () => {
-        it('it should GET data of map', (done) => {
-          chai.request(server)
-              .get('/api/datasets/p_parque17/map')
-              .end((err, res) => {
-                    res.should.have.status(200);
-                done();
-              });
+  describe('/GET data of map', function() {
+        it('it should GET data of map', async() => {
+          var response = await fetch('http://localhost:3000/api/datasets/m_parque16/mapa')
+          expect(response.status).to.be.equal(200);
+          var d = await response.json();
+          var dashboard = [];
+          for (var i in d){
+              dashboard.push(i);
+          }
+          expect(dashboard[0]).to.equals("layers");
+          expect(dashboard[1]).to.equals("geom");
         });
   });
 
-  describe('/GET items of Dataset', () => {
-        it('it should GET items of Dataset', (done) => {
-          chai.request(server)
-              .get('/api/datasets/p_parque17/items/28')
-              .end((err, res) => {
-                    res.should.have.status(200);
-                done();
-              });
+  describe('/GET items of Dataset', function() {
+        it('it should GET items of Dataset', async() => {
+          var response = await fetch('http://localhost:3000/api/datasets/p_poblacion17/items/28')
+          expect(response.status).to.be.equal(200);
+          var d = await response.json();
+
+          var response = await fetch('http://localhost:3000/api/datasets/p_poblacion17/data/id/28')
+          expect(response.status).to.be.equal(200);
+          var d_1 = await response.json();
+          expect(d_1[0].cod).to.equals('28');
+          expect(d_1[0].año).to.equals('2017');
+
+          expect(d_1[0].hombres).to.equals(d[0].hombres);
+          expect(d_1[0].mujeres).to.equals(d[0].mujeres);
+
+
         });
+
   });
 
 
