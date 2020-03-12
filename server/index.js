d63f8c8f44c05a2005f24b1f723b72801f3a613c
Rubén Béjar <rbejar@unizar.es>
2020-03-12 14:18:07 +0100

    Quick fix for the CORS policy. Not ready for production.

--- server/index.js
+++ server/index.js
@@ -8,6 +8,12 @@ const server = http.createServer(app);
 
 const router = require('./routes/index');
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(bodyParser.json())
 app.use(
   bodyParser.urlencoded({
@@ -23,6 +29,9 @@ app.use((req, res, next) => {
 });
 
 app.use('/api',router);
+
+
+
 
 
 if(!module.parent){ app.listen(port); }
